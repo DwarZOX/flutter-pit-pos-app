a192ea97e1238e251ce602422c556fae93192bd9
edwards <zedwar19@gmail.com>
2024-01-11 15:08:03 +0700

    feat: setup login page

---
+++ lib/core/constants/variables.dart
@@ -0,0 +1,3 @@
+class Variables {
+  static const String baseUrl = 'http://192.168.176.11:8000';
+}
---
+++ lib/data/datasources/auth_local_datasource.dart
@@ -0,0 +1,10 @@
+import 'package:pit_pos_app/data/models/response/auth_response_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AuthLocalDatasource {
+  Future<void> saveAuthData(AuthResponseModel authResponseModel) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('auth_data', authResponseModel.toJson());
+  }
+
+}
